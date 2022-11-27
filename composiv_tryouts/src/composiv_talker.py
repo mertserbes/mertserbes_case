07a64f0b86606431896ca321ea5d7f1a3ae84ead
mertserbes <72387579+mertserbes@users.noreply.github.com>
2022-11-27 15:49:18 +0300

    Update composiv_talker.py

--- composiv_tryouts/src/composiv_talker.py
+++ composiv_tryouts/src/composiv_talker.py
@@ -4,9 +4,9 @@ import rospy
 from std_msgs.msg import String
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    pub = rospy.Publisher('chatter', String, queue_size=10) #Inside the speaker function, it first creates a publisher object. 
+    rospy.init_node('talker', anonymous=True)               #The first parameter is the topic name, the second parameter is the topic message type, 
+    rate = rospy.Rate(10) # 10hz                            #and the third parameter is the amount of messages accumulated until the memory is cleared.
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
         rospy.loginfo(hello_str)
